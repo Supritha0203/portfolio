5539266b2a8d688778f9fa571668d53b440bcd52
SuprithaRao1302 <245120733077@mvsrec.edu.in>
2024-05-14 13:21:31 +0530

    resolve spellings

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -6,7 +6,7 @@ import { HiArrowRight } from "react-icons/hi";
 import "./Blog.css";
 import { ThemeContext } from "../../contexts/ThemeContext";
 import { blogData } from "../../data/blogData";
-import SingleBlog from "./SingleBlog/Singleblog";
+import SingleBlog from "./SingleBlog/SingleBlog";
 
 function Blog() {
   const { theme } = useContext(ThemeContext);
